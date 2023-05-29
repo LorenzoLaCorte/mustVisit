5e30cb43058e5978b2da90c78c45b480bb2828c9
lax <lorenzolacorte99@gmail.com>
2023-05-29 16:06:42 +0200

    main activity UI

--- app/src/main/java/com/example/mustvisit/MainActivity.java
+++ app/src/main/java/com/example/mustvisit/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mustvisit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void showResults(View v) {
+        // TODO: collect:
+        //  - the position (x,y) of the user
+        //  - the range (range) selected
+        //  - the categories ([categories]) selected
+        //  and send these values to the other activity
+        Intent i = new Intent(this, Results.class);
+        startActivity(i);
     }
 }
